f7395b618235d110c8ecaf258b01a06386a7936b
bryannguyeen <bryan.nguyen@ucalgary.ca>
2017-10-28 12:27:11 -0600

    Worked on ChooseProfilePic

--- kidZone/ChooseProfilePic.xaml.cs
+++ kidZone/ChooseProfilePic.xaml.cs
@@ -24,5 +24,10 @@ namespace kidZone
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
